64dd1a3521c70e9ab5ccae56c6c9fd4ee27b8612
ThisChessPlayer <jasonma5501@gmail.com>
2017-02-24 12:30:01 -0800

    got a time control selector to popup and communicate with class variable. also styled it more appropriately and added a Constants class for time constants

---
+++ app/src/main/java/com/example/android/chessclock/Constants.java
@@ -0,0 +1,12 @@
+package com.example.android.chessclock;
+
+/**
+ * Created by JasonMa on 2/24/2017.
+ */
+
+public class Constants {
+  //millis factors
+  final static long HOUR = 3600000;
+  final static long MINUTE = 60000;
+  final static long SECOND = 1000;
+}
--- app/src/main/java/com/example/android/chessclock/TimeSelectorActivity.java
+++ app/src/main/java/com/example/android/chessclock/TimeSelectorActivity.java
@@ -1,12 +1,25 @@
 package com.example.android.chessclock;
 
 import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import android.app.DialogFragment;
-import android.widget.TimePicker;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.NumberPicker;
+
+import android.widget.Button;
+
+import android.view.View;
+
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+
 
 /**
  * Created by JasonMa on 2/24/2017.
@@ -14,24 +27,69 @@ import android.widget.TimePicker;
 
 public class TimeSelectorActivity extends AppCompatActivity {
 
+  static int playerTopTime;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_time_selector);
 
-
+    Button topTime = (Button) findViewById(R.id.player1Time);
+    topTime.setOnClickListener(onclick);
 
   }
 
-  public static class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+  View.OnClickListener onclick =
+        new View.OnClickListener(){
+          @Override
+          public void onClick(View view){
+            NumberPickerFragment topTimeFragment = new NumberPickerFragment();
+            topTimeFragment.show(getFragmentManager(), "Top Time");
+          }
+        };
+
+  public static class NumberPickerFragment extends DialogFragment implements NumberPicker.OnValueChangeListener {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-      return super.onCreateDialog(savedInstanceState);
+      //return super.onCreateDialog(savedInstanceState);
+      LayoutInflater inflater = getActivity().getLayoutInflater();
+
+      View v = inflater.inflate(R.layout.time_selector_hms, null);
+      final NumberPicker hourPicker = (NumberPicker) v.findViewById(R.id.hourPicker);
+      final NumberPicker minPicker = (NumberPicker) v.findViewById(R.id.minPicker);
+      final NumberPicker secPicker = (NumberPicker) v.findViewById(R.id.secPicker);
+      hourPicker.setMaxValue(99);
+      hourPicker.setMinValue(0);
+      minPicker.setMaxValue(59);
+      minPicker.setMinValue(0);
+      secPicker.setMaxValue(59);
+      secPicker.setMinValue(0);
+
+      return new AlertDialog.Builder(getActivity(), R.style.AlertDialogCustomTheme)
+            .setTitle("Set Player 1 Time")
+            .setView(v)
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+                playerTopTime = 0;
+                playerTopTime += hourPicker.getValue() * Constants.HOUR;
+                playerTopTime += minPicker.getValue() * Constants.MINUTE;
+                playerTopTime += secPicker.getValue() * Constants.SECOND;
+                Log.d("jaslog", "PlayerTopTime: " + playerTopTime);
+              }
+            })
+            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+              @Override
+              public void onClick(DialogInterface dialog, int which) {
+
+              }
+            })
+            .create();
     }
 
     @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 
     }
   }
